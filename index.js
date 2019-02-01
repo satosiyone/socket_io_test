e93ad53f4e72bc7438559f1039136b8ddbb46232
yone <yone@yone.local>
2019-02-01 18:07:16 +0900

    socket.io tutorial complete

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
+// everyoneにsocket.ioのイベント発火(emit)
+io.emit('some event',{for: 'everyone' });
+
+// 全員にメッセージをブロードキャスト
 io.on('connection', function(socket){
-    console.log('a user connected!');
+    socket.broadcast.emit('hi');
+});
+
+// 同時全員送信
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        // クライアント側からの'chat message'を受け取る
+        io.emit('chat message', msg);
+    })
 })
 
 http.listen(3000, function(){
