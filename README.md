36ad2c65bb941cc0b4db7b9a3e13090798c0803b
yone <yone@yone.local>
2019-02-05 15:27:23 +0900

    add README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # socket_io_test
+
+# 設計
+- Nodeサーバにアクセス
+    - .js
+        - app.get で index.htmlに転送
+    - .html
+        - socket.io.jsが読み込まれる
+        - ログインフォームが表示
+- ログインボタン押下
+    - .html
+        - チャットフォームが表示
+        - `ユーザ情報` 送信
+            - emit `login`
+                - userID: socket.id // io()でidが取得できる
+                - userName: userName
+    - .js
+        - on `connection`
+            - `ユーザ情報` 受け取る
+                - on `login`
+                    - loginUsers[ID]に名前を保存
+- チャット送信時
+    - .html
+        - メッセージを送信
+            - emit `chat message`
+    - .js
+        - メッセージを受け取る
+            - on `chat message`
+                - `socket` に紐付く `ID` でユーザ名を特定
+                - `ユーザ情報` を送信
+                    - emit `chat message`
+    - .html
+        - ユーザ情報とメッセージを受け取る
+            - on `chat message`
+                - ユーザ情報とメッセージを表示
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 var app = require('express')();
-var http = require('http').Server(app);
+var http  = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // Nodeサーバにアクセスがあるとindex.htmlへ遷移
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
-
-/*
-// everyoneにsocket.ioのイベント発火(emit)
-io.emit('some event',{for: 'everyone' });
-
-// 全員にメッセージをブロードキャスト
-io.on('connection', function(socket){
-    socket.broadcast.emit('hi');
-});
-*/
 
 // ログイン
 io.on('connection', function(socket){
@@ -35,16 +25,6 @@ io.on('connection', function(socket){
         });
     });
 });
-
-// 同時全員送信
-/*
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        // クライアント側からの'chat message'を受け取る
-        io.emit('chat message', msg);
-    })
-})
-*/
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
