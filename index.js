cd7e318f2672523fc345c462d254cf7cc893cc04
yone <yone@yone.local>
2019-02-18 19:26:18 +0900

    Map化完了およびプロトタイプ完成

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ var http  = require('http').Server(app);
 var io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
 //var loginUsers = []; // この書き方だと SS -> CS へ emit するときに undefined になる // 半日ハマった
-var loginUsers = {}; //ログインユーザ // connection内で宣言すると毎回初期化される
-var answers = {}; // 回答
-var votes = {}; // 投票
-
-// たぶんデータ構造こうしたほうがいい
-// objList= {id, A/Bどちら回答したか, 誰に投票したか}
-// sample = { XAIOFE3eF :
-//           (A, XJSAFI9sajd)
-//          }
+var idMap = new Map();
 
 // Nodeサーバにアクセスがあるとindex.htmlへ遷移
 app.get('/', function(req,res){
@@ -22,14 +14,22 @@ app.get('/', function(req,res){
 io.on('connection', function(socket){
     // ログイン処理
     socket.on('login', function(userInfo){
-        loginUsers[userInfo.userID] = userInfo.userName;
+        let obj = {name: userInfo.userName, answer: 'yet', vote: 'yet', counts: 0};
+        idMap.set(userInfo.userID, obj);
         // ユーザリスト表示
-        io.emit('user list', loginUsers);
+        let htmlStr = '<ul>';
+        idMap.forEach(function(value, key){
+            htmlStr += '<li id=list_' + key + '>' + value.name + '</li>';
+            htmlStr += '<button class=btnVote id=btn_' + key + '>投票</button>';
+            htmlStr += '<button class=btnHang id=hang_' + key + '>正体を暴く</button>';
+        });
+        htmlStr += '</ul>';
+        io.emit('user list', htmlStr);
     });
 
     // メッセージ送信処理
     socket.on('chat message', function(msg){
-        userName = loginUsers[socket.id];
+        userName = idMap.get(socket.id).name;
         io.emit('chat message',{
             userName: userName,
             message: msg
@@ -39,7 +39,7 @@ io.on('connection', function(socket){
     // STARTボタン押下時
     socket.on('btnStart',function(){
         let htmlStr = '';
-        let odai = ['A:夏 B:冬', 'A:たけのこ B:キノコ', 'A:父親 B:母親'];
+        let odai = ['A:夏 B:冬', 'A:たけのこ B:キノコ', 'A:父親 B:母親', 'A:数字の２ B:数字の３'];
         let rand = Math.floor(Math.random() * 3);
         htmlStr += odai[rand];
         //お題表示
@@ -49,21 +49,21 @@ io.on('connection', function(socket){
     // A/Bボタン押下時
     socket.on('btnA',function(id){
         io.emit('btnA', id);
-        answers[id] = 'A';
+        idMap.get(id).answer = 'A';
     });
 
     socket.on('btnB',function(id){
         io.emit('btnB', id);
-        answers[id] = 'B';
+        idMap.get(id).answer = 'B';
     });
 
     // 集計ボタン押下時
     socket.on('btnResults',function(){
         let counts = {};
-        for(let id in answers){
-            let key = answers[id];
+        idMap.forEach(function(value){
+            let key = value.answer;
             counts[key] = (counts[key]) ? counts[key] + 1 : 1;
-        };
+        });
         let htmlStrA = 'A: ' + (counts['A'] ? counts['A'] : '0');
         let htmlStrB = ' B: ' + (counts['B'] ? counts['B'] : '0');
         io.emit('btnResults', htmlStrA + htmlStrB);
@@ -71,40 +71,32 @@ io.on('connection', function(socket){
 
     // リセットボタン押下時
     socket.on('btnReset',function(){
-        loginUsers = {};
-        answers = {};
+        idMap = new Map();
     });
 
     // 投票ボタン押下時
     socket.on('btnVote',function(voteFrom, voteTo){
         // key:投票した人 value:投票相手
-        votes[voteFrom] = voteTo;
+        idMap.get(voteFrom).vote = voteTo;
+    });
+
+    // 暴くボタン押下時
+    socket.on('btnHang',function(id){
+        io.emit('btnHang', id, idMap.get(id).answer);
     });
 
     // 開票結果ボタン押下時
     socket.on('btnOpen',function(){
         let counts = {};
-        let htmlStr = ''
-        for(let i in votes){
-            // 投票した人のIDの数だけループ
-            // 投票相手のIDをkeyに入れる
-            let key = votes[i];
+        idMap.forEach(function(value){
+            let key = value.vote;
             counts[key] = (counts[key]) ? counts[key] + 1 : 1;
-        };
-        
-        /*test 
-        for(let id of Object.keys(counts)){
-            // id は 投票された人のID
-            console.log(' id = ' + id + ' counts=' + counts[id]);
-        };
-        */
+        });
 
-        // votes = {aID: bID, bID:aID, cID:bID};
-        // A:1 B:2 C:0 
-        for(let id in loginUsers){
-            htmlStr += loginUsers[id] + ' : ' + counts[id] + ' ';
-        };
-        console.log('htmlStr=' + htmlStr);
+        let htmlStr = '';
+        idMap.forEach(function(value, key){
+            htmlStr += value.name + ' : ' + (counts[key] ? counts[key] : '0') + ' ';
+        });
         io.emit('btnOpen', htmlStr);
     });
 });
